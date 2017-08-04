4cdc45986be05583f3c96ec6ab21a378f692d40e
YeRin Kim <99101022@naver.com>
2017-08-04 15:41:55 +0900

    initial import

---
+++ app/src/main/java/push/example/a99101/goodweather/FourthActivity.java
@@ -0,0 +1,22 @@
+package push.example.a99101.goodweather;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 99101 on 2017-08-02.
+ */
+
+public class FourthActivity extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fourth_activity, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/push/example/a99101/goodweather/SecondActivity.java
@@ -0,0 +1,18 @@
+package push.example.a99101.goodweather;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondActivity extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.second_activity, container, false);
+        return v;
+    }
+}
